c05d5c44630d079bc2f94b89fa50c90eea7027ef
aseara <aseara@163.com>
2016-03-01 10:47:58 +0800

    ch 8 Listing 8.2 · LongLines with a local processLine function.

--- src/main/scala/com/aseara/scala/ch8/LongLines.scala
+++ src/main/scala/com/aseara/scala/ch8/LongLines.scala
@@ -13,14 +13,14 @@ object LongLines {
 
   def processFile(filename: String, width: Int) {
 
+    def processLine(line: String) {
+      if (line.length > width)
+        println(filename +": "+ line.trim)
+    }
+
     val source = Source.fromFile(filename)
     for (line <- source.getLines())
-      processLine(filename, width, line)
-  }
-
-  private def processLine(filename: String, width: Int, line: String) {
-    if (line.length > width)
-      println(filename +": "+ line.trim)
+      processLine(line)
   }
 
 }
