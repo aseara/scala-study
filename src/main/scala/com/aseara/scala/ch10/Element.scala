27d54bbe3b6c09351f95b52ec10ac23145c35b1e
aseara <aseara@163.com>
2016-03-02 14:05:01 +0800

    ch10 Listing 10.3 · Defining ArrayElement as a subclass of Element.

--- src/main/scala/com/aseara/scala/ch10/Element.scala
+++ src/main/scala/com/aseara/scala/ch10/Element.scala
@@ -7,7 +7,12 @@ package com.aseara.scala.ch10
  * Time: 10:46
  */
 
-
 abstract class Element {
   def contents: Array[String]
+  def height: Int = contents.length
+  def width: Int = if (height == 0) 0 else contents(0).length
+}
+
+class ArrayElement(conts: Array[String]) extends Element {
+  def contents: Array[String] = conts
 }
